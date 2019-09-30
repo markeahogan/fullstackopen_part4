a172ac13282d7c12ff99df5e1be6a78ce38923b3
Mark Hogan <markeahogan@gmail.com>
2019-09-30 20:53:03 +0000

    4.1 working nom project

--- bloglist/index.js
+++ bloglist/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const http = require('http');
 const express = require('express');
 const app = express();
@@ -14,7 +15,7 @@ const blogSchema = mongoose.Schema({
 
 const Blog = mongoose.model('Blog', blogSchema);
 
-const mongoUrl = 'mongodb://localhost/bloglist';
+const mongoUrl = process.env.MONGODB_URI;
 mongoose.connect(mongoUrl, {useNewUrlParser: true});
 
 app.use(cors());
@@ -29,7 +30,7 @@ app.get('/api/blogs', (req, res) => {
 });
 
 app.post('/api/blogs', (req, res) => {
-    const blog = new Blog(request.body);
+    const blog = new Blog(req.body);
 
     blog
     .save()
@@ -38,7 +39,7 @@ app.post('/api/blogs', (req, res) => {
     });
 });
 
-const PORT = 3003;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
