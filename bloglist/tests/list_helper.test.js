077b7f1722e38c6ac7b9418607b495882363592e
Mark Hogan <markeahogan@gmail.com>
2019-10-01 19:21:19 +0000

    4.5 favorite blog

--- bloglist/tests/list_helper.test.js
+++ bloglist/tests/list_helper.test.js
@@ -17,3 +17,18 @@ describe('total likes', ()=> {
         expect(result).toBe(5);
     });
 });
+
+describe('favorite blog', ()=> {
+    const listWithTwoBlogs = [
+        {
+            likes:12
+        },
+        {
+            likes:15
+        }];
+
+    test('favorite blogs has most likes', () => {
+        const result = listHelper.favoriteBlog(listWithTwoBlogs);
+        expect(result).toBe(listWithTwoBlogs[1]);
+    });
+});
--- bloglist/utils/list_helper.js
+++ bloglist/utils/list_helper.js
@@ -6,7 +6,12 @@ const totalLikes = (blogs) => {
     return blogs.reduce((a, c) => a += c.likes, 0);
 }
 
+const favoriteBlog = (blogs) => {
+    return blogs.reduce((a, c) => c.likes > a.likes ? c : a);
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
