fcbe08d3b596079de684e46432989710cca4dbc7
Mark Hogan <mark@hiddenltd.com>
2019-10-11 14:04:47 +0100

    start on 4.11

--- bloglist/tests/blog_api.test.js
+++ bloglist/tests/blog_api.test.js
@@ -22,29 +22,54 @@ beforeEach(async () => {
     }
 })
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-})
-
-test('all blogs are returned', async () => {
-    const response = await api.get('/api/blogs')  
+test('the blog list application returns the correct amount of blog posts in the JSON format', async () => {
+    const response = await api
+        .get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+        
     expect(response.body.length).toBe(helper.initialBlogs.length)
 })
 
-test('blogs have id', async () => {
+test('the unique identifier property of the blog posts is named id', async () => {
     const response = await api.get('/api/blogs')  
     expect(response.body[0].id).toBeDefined()
 })
 
-test('blogs are returned as json', async () => {
-    await api
-      .get('/api/blogs')
-      .expect(200)
-      .expect('Content-Type', /application\/json/)
-  })
+test('making an HTTP POST request to the /api/blogs url successfully creates a new blog post', async () => {
+    const newBlog = {
+        title: 'new blog',
+        author: 'new author',
+        url:'new url',
+        likes:'0'
+    }
+    
+    await api.post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+    const newBlogs = await helper.getBlogs()
+    expect(newBlogs.length).toBe(helper.initialBlogs.length + 1)  
+
+    expect(newBlogs[newBlogs.length-1].title).toBe(newBlog.title);
+})
+
+test('if the likes property is missing from the request, it will default to the value 0', async () => {
+    const newBlog = {
+        title: 'new blog',
+        author: 'new author',
+        url:'new url'
+    }
+    
+    await api.post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+    const newBlogs = await helper.getBlogs()
+    expect(newBlogs[newBlogs.length-1].likes).toBe(0);
+})
 
 afterAll(async () => {
   mongoose.connection.close()
--- bloglist/tests/test_helper.js
+++ bloglist/tests/test_helper.js
@@ -15,19 +15,11 @@ const initialBlogs = [
     }
 ]
 
-const nonExistingId = async () => {
-  const note = new Note({ content: 'willremovethissoon' })
-  await note.save()
-  await note.remove()
-
-  return note._id.toString()
-}
-
-const notesInDb = async () => {
-  const notes = await Note.find({})
-  return notes.map(note => note.toJSON())
+const getBlogs = async () => {
+  const blogs = await Blog.find({})
+  return blogs.map(blog => blog.toJSON())
 }
 
 module.exports = {
-    initialBlogs, nonExistingId, notesInDb
+    initialBlogs, getBlogs
 }
